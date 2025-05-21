fd112fbf4d25b7fae057a30c61e4fd1325107e09
Florent Monnier <monnier.florent@gmail.com>
2025-05-21 21:39:17 +0200

    commands to apply a mask and crop

---
+++ extract.ml
@@ -0,0 +1,20 @@
+let imgs = [
+  "recieved/Screenshot 2024-07-04 09.42.02.png",
+    "masks/Screenshot 2024-07-04 09.42.02.png";
+]
+
+let apply_mask img mask =
+  Printf.sprintf
+    "convert '%s' '%s' -alpha off -compose CopyOpacity -composite output.png"
+    img mask
+
+let crop x y w h =
+  Printf.sprintf
+    "convert output.png -crop %dx%d+%d+%d +repage cropped.png"
+    w h x y
+
+let () =
+  List.iter (fun (recieved, mask) ->
+    print_endline (apply_mask recieved mask)
+  ) imgs;
+;;
