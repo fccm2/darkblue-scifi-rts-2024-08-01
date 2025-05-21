8ea91f8c936b928d6b05f4b9338847d4e896d44a
Florent Monnier <monnier.florent@gmail.com>
2025-05-21 21:42:07 +0200

    add a crop area

--- extract.ml
+++ extract.ml
@@ -1,6 +1,9 @@
 let imgs = [
   "recieved/Screenshot 2024-07-04 09.42.02.png",
-    "masks/Screenshot 2024-07-04 09.42.02.png";
+    "masks/Screenshot 2024-07-04 09.42.02.png",
+    [
+      ["x", 173; "y", 124; "w", 54; "h", 33];
+    ];
 ]
 
 let apply_mask img mask =
@@ -15,6 +18,6 @@ let crop x y w h =
 
 let () =
   List.iter (fun (recieved, mask) ->
-    print_endline (apply_mask recieved mask)
+    print_endline (apply_mask img mask)
   ) imgs;
 ;;
